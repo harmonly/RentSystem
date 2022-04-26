e1ed0e0676f9294d2dbde40c3086a669419a9cac
harmonly <bullniumao@outlook.com>
2022-04-26 10:57:29 +0800

    add MainFilter

---
+++ src/main/java/com/example/rentsystem/MainFilter.java
@@ -0,0 +1,28 @@
+package com.example.rentsystem;
+
+import com.example.rentsystem.entity.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class MainFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String url = req.getRequestURL().toString();
+        if (!url.contains("/assets/") && !url.endsWith("login")) {
+            HttpSession session = req.getSession();
+            User user = (User) session.getAttribute("user");
+            if (user == null) {
+                res.sendRedirect("login");
+                return;
+            }
+        }
+        chain.doFilter(req, res);
+    }
+}
